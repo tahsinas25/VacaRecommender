ca8258e8b48180c3ffae27e33213defe59583c1f
mmim14 <87958743+mmim14@users.noreply.github.com>
2024-01-28 09:51:42 -0500

    Structured
    Restructured the project.

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const api_key = require('./credentials.json')
 const port = 3000;
 
 const server = http.createServer();
-server.on("request", request_handler);   // any request for the data 
+server.on("request", request_handler);   // any request for the data; will be executed every time a search occurs
 server.on("listening", listen_handler);
 server.listen(port);
 
@@ -13,45 +13,52 @@ function listen_handler(){
 	console.log(`Now Listening on Port ${port}`);
 }
 function request_handler(req, res){
-    console.log(req.url);
     if(req.url === "/"){
-        const form = fs.createReadStream("index.html");
-		res.writeHead(200, {"Content-Type": "text/html"})
+        const form = fs.createReadStream("../Frontend/index.html");  // reads the index.html file
+		res.writeHead(200, {"Content-Type": "text/html"});  // 
 		form.pipe(res);
     }
     else if(req.url.startsWith("/search")){
-        let key = api_key[0]["API-KEY"] 
         const user_input = new URL(req.url, `https://${req.headers.host}`).searchParams;     
-        console.log(user_input)
-        const location = user_input.get('location');   // gets location
+        const location = user_input.get('city');   // gets location
+        console.log(location)
         if(location == null || location == ""){
             res.writeHead(404, {"Content-Type": "text/html"}); 
             res.end("<h1>Missing Input</h1>");        
         }
         else{
+            let key = api_key[0]["API-KEY"] 
             const options = {
                 method: 'GET',
                 hostname: 'ai-trip-planner.p.rapidapi.com',
                 port: null,
-                path: `/?days=3&destination=${location}`,
+                path: `/?days=3&destination=${encodeURIComponent(location)}`,
                 headers: {
                     'X-RapidAPI-Key': key,
                     'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
                 }
             };
-            const vacations = https.request(options, (res) => {
+
+            let body = '';
+            // requesting the vacation plan from the API 
+            const vacations = https.request(options, (response) => {
                 const chunks = [];
-	            res.on('data', function (chunk) {
+	            response.on('data', function (chunk) {
 		            chunks.push(chunk);
 	            });
 
-	            res.on('end', function () {
-		            const body = Buffer.concat(chunks);
-		            console.log(body.toString());
+	            response.on('end', function () {
+		            body = Buffer.concat(chunks);
+                    let plan = `<p>${body}</p>`
+                    res.write("<h1>Trip Plan</h1>", () => {});
+                    res.write(plan, () => {});
+                    res.end()
                 });  
-            
             });
+            vacations.end()  // end the API request call
+            // res is a writeable stream 
         }
+
     }
 
     else{
