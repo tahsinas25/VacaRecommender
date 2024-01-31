7a6a4f7ac1c8efaaac3a03cfa533810f9a4c85bd
mmim14 <87958743+mmim14@users.noreply.github.com>
2024-01-30 20:55:53 -0500

    Integrated frontend and backend code

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');    // for hosting our server
 const https = require('https');  // for the API
+const cheerio = require('cheerio')
 const api_key = require('./credentials.json')
 const port = 3000;
 
@@ -12,7 +13,9 @@ server.listen(port);
 function listen_handler(){
 	console.log(`Now Listening on Port ${port}`);
 }
-function request_handler(req, res){
+
+function request_handler(req, res) {
+
     if(req.url === "/"){
         const form = fs.createReadStream("../Frontend/index.html");  // reads the index.html file
 		res.writeHead(200, {"Content-Type": "text/html"});  // 
@@ -21,7 +24,6 @@ function request_handler(req, res){
     else if(req.url.startsWith("/search")){
         const user_input = new URL(req.url, `https://${req.headers.host}`).searchParams;     
         const location = user_input.get('city');   // gets location
-        console.log(location)
         if(location == null || location == ""){
             res.writeHead(404, {"Content-Type": "text/html"}); 
             res.end("<h1>Missing Input</h1>");        
@@ -32,7 +34,7 @@ function request_handler(req, res){
                 method: 'GET',
                 hostname: 'ai-trip-planner.p.rapidapi.com',
                 port: null,
-                path: `/?days=3&destination=${encodeURIComponent(location)}`,
+                path: `/?days=1&destination=${encodeURIComponent(location)}`,
                 headers: {
                     'X-RapidAPI-Key': key,
                     'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
@@ -49,20 +51,42 @@ function request_handler(req, res){
 
 	            response.on('end', function () {
 		            body = Buffer.concat(chunks);
-                    let plan = `<p>${body}</p>`
-                    res.write("<h1>Trip Plan</h1>", () => {});
-                    res.write(plan, () => {});
-                    res.end()
+                    plan = `<p>${body}</p>`
+                    console.log(plan)
+                    process_plan(plan)
                 });  
             });
             vacations.end()  // end the API request call
             // res is a writeable stream 
         }
-
     }
-
     else{
         res.writeHead(404, {"Content-Type": "text/html"});
         res.end("<h1>Not Found</h1>");    
+    }
+
+
+    function process_plan(response) {
+        res.write("<h1>Trip Plan</h1><break><break><break><p>You're Welcome!</p>", () => {});
+        let body = cheerio.load(response)('p').text();
+        try {
+            body = JSON.parse(body)
+            body = body.plan  // array of plans 
+
+        } catch (err) {
+            console.log('Error parsing JSON:', err)
+        }
+        // process the plan array 
+        let trip = ''
+        for(i = 0; i < body.length; i++) {
+            trip += `Day: ${body[i]['day']} \n`
+            let activities = body[i]['activities']  // json object containing all the activities
+            for(j = 0; j < activities.length; j++) {
+                trip += `Time: ${activities[j]['time']} \n Description: ${activities[j]['description']}\n`
+            }
+        }
+        console.log(trip)
+        res.write(trip, () => {});
+        res.end()
     }
 }
